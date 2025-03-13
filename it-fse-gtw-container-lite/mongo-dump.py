e02f2ba063d7a0a4c0aa3194bf9ccafb829de462
Alberto Bosisio <albertobosisio@3caravelle.com>
2025-03-13 14:21:17 +0100

    fix: corretto script per gestione dataset gzipped

--- it-fse-gtw-container-lite/mongo-dump.py
+++ it-fse-gtw-container-lite/mongo-dump.py
@@ -1,6 +1,5 @@
-from io import BytesIO
-from os import getenv, path, mkdir
-from shutil import rmtree
+from os import getenv, path, mkdir, remove
+from shutil import rmtree, copyfileobj
 from urllib.request import urlopen
 from dotenv import load_dotenv
 import gzip
@@ -9,12 +8,7 @@ load_dotenv()
 
 # VARIABLES
 MONGO_DUMP_URL = getenv("MONGO_DUMP_URL")
-FILES = ["schema.json.gzip", 
-        "schematron.json.gzip",
-        "terminology.json.gzip",
-        "transform.json.gzip",
-        "engines.json.gzip"]
-
+FILES = ["schema.json.gzip", "schematron.json.gzip", "terminology.json.gzip", "transform.json.gzip"]
 SCRIPT_DIR = path.dirname(path.abspath(__file__))
 FOLDER = path.join(SCRIPT_DIR, "../mongo-dump")
 
@@ -22,7 +16,7 @@ if MONGO_DUMP_URL is None:
     raise ValueError("MONGO_DUMP_URL environment variable is not set in .env")
 
 # CHECK if folder already exists, if yes it will be deleted
-if(path.exists(FOLDER)):
+if path.exists(FOLDER):
     rmtree(FOLDER)
     print(" - mongo-dump folder already exists. It will be deleted and recreated.")
 
@@ -31,28 +25,25 @@ mkdir(FOLDER)
 print(" - mongo-dump folder has been created.")
 
 for file in FILES:
-
     url = f"{MONGO_DUMP_URL}/{file}"
-
-    # REQUEST the file on GitHub
-    print(f"{file}".upper())
+    print(f"{file.upper()}")
     print(f" - HTTP request for {file} is starting: {url}")
-    contents = urlopen(url)
+    response = urlopen(url)
     print(f" - {file} is downloaded.")
 
-    # Read the compressed content
-    compressed_data = contents.read()
+    # Salva il file compresso
+    compressed_path = path.join(FOLDER, file)
+    with open(compressed_path, "wb") as f:
+        print(f" - {file} in writing...")
+        f.write(response.read())
+        print(f" - {file} is written on disk.")
 
-    # Decompress the gzip content
-    with gzip.GzipFile(fileobj=BytesIO(compressed_data)) as gz:
-        decompressed_data = gz.read() 
-
-    # Save the decompressed data as .json
-    output_filename = file.replace(".gzip", "")
-
-    # OPEN the file and write on it
-    with open(f"{FOLDER}/{output_filename}", "wb") as f:
-        print(f" - {output_filename} in writing...")
-        f.write(decompressed_data)
-        print(f" - {output_filename} is written on disk.")
-        f.close()
+    # Decomprimi il file: il nome del file decomprimato sarà lo stesso senza l'estensione .gzip
+    decompressed_filename = file.replace(".gzip", "")
+    decompressed_path = path.join(FOLDER, decompressed_filename)
+    with gzip.open(compressed_path, 'rb') as f_in:
+        with open(decompressed_path, 'wb') as f_out:
+            copyfileobj(f_in, f_out)
+    print(f" - {file} is decompressed to {decompressed_filename}.")
+    remove(compressed_path)
+    print(f" - {file} compressed file is removed.")
